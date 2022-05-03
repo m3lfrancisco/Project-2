586861302bad2efbfc17ffec76d9e595e390215f
mfrancisco <francisco.melanie@gmail.com>
2022-05-03 13:06:47 -0700

    install, require, configure and mount session middleware, add SECRET to .env file, receive successful connection message to mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 require('dotenv').config();
 require('./config/database');
 var methodOverride = require('method-override');
@@ -15,11 +16,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
