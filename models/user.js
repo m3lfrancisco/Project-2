03201b8e1dd45f1cec3daf2fca07f261aac9dc2b
mfrancisco <francisco.melanie@gmail.com>
2022-05-03 13:43:20 -0700

    install oauth strategy and configure passport

--- models/user.js
+++ models/user.js
@@ -8,8 +8,19 @@ const userSchema = new Schema({
         required: true
     },
     email: String,
+    avatar: String
 }, {
     timestamps: true
+});
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user._id);
+});
+
+passport.deserializeUser(function(userId, cb) {
+    User.findById(userId).then(function(user) {
+        cb(null, user);
+    });
 });
 
 module.exports = mongoose.model('User', userSchema);
